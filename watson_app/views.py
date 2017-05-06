04107bde33c6f7fc5e160ee14d09f4cdca02126c
Anthony Georgiades <anthonygeorgiades@gmail.com>
2017-05-06 16:12:12 -0400

    Updated views.py

--- watson_app/views.py
+++ watson_app/views.py
@@ -13,6 +13,7 @@ from watson_developer_cloud import ToneAnalyzerV3
 
 
 # Watson authentication
+#here is a comment
 tone_analyzer = ToneAnalyzerV3(
     username='afad9d73-df3e-41c8-976c-9c505fa2dd12',
     password='SPBJxlMsVrl0',
